7b840fb55d2c220633f63196038d5ed34e7cbb61
Liuyifeii <yifeiliu@zju.edu.cn>
2025-04-07 13:31:48 +0800

    Delete filter directory

--- filter/aromatic_ring_filter.py
+++
@@ -1,8 +0,0 @@
-from rdkit.Chem import Descriptors, rdMolDescriptors
-
-from filter.filter import Filter
-
-
-class AromaticRingFilter(Filter):
-    def check(mol, conf):
-        return Descriptors.NumAromaticRings(mol) > 0
